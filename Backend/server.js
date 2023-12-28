1627e18f04bc2f84ea6f223649c79f50375dc5a3
isaak gz <yesakgz12@gmail.com>
2023-12-28 05:37:34 -0300

    back end setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import products from "./data/products.js";
 const port = 5000;
+
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("wellocme");
 });
 
+app.get("/api/products", (req, res)=>{
+  res.json(products)
+})
 
+app.get("/api/products/:id", (req, res)=>{
+  const product = products.find((p)=>p._id ===req.params.id)
+  res.send(product)
+})
 
 app.listen(port, ()=>{
     console.log(`server is listing on ${port}`)
