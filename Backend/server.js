7fd834f9ede83a67e70546945267cc086dd204c9
isaak gz <yesakgz12@gmail.com>
2023-12-28 05:52:49 -0300

    set enviromrnt varibles

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import products from "./data/products.js";
-const port = 5000;
-
+const port = process.env.PORT || 8000;
 
 const app = express();
 
@@ -9,15 +10,15 @@ app.get("/", (req, res) => {
   res.send("wellocme");
 });
 
-app.get("/api/products", (req, res)=>{
-  res.json(products)
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get("/api/products/:id", (req, res)=>{
-  const product = products.find((p)=>p._id ===req.params.id)
-  res.send(product)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.send(product);
+});
 
-app.listen(port, ()=>{
-    console.log(`server is listing on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is listing on ${port}`);
+});
