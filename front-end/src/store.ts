8bd953b6e1e3cc621a7b191be9c63802173473f0
isaak gz <yesakgz12@gmail.com>
2024-01-04 14:51:05 -0300

    create apiSlice

--- front-end/src/store.ts
+++ front-end/src/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-
+import { apiSlice } from "./features/apiSclices/apiSclices";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
-
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
