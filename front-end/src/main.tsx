b16931b674d7da53a598e67169c95389c5532604
isaak gz <yesakgz12@gmail.com>
2024-01-04 08:04:08 -0300

    redux toolkit setup and configuring store and connect to app component

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store.ts";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App.tsx";
 import "./index.css";
@@ -16,14 +18,15 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
-      <Route  path="/product/:id" element={<ProductPage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
