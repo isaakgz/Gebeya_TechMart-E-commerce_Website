ff025f680444e6e05b12359d05646b48378b1aba
isaak gz <yesakgz12@gmail.com>
2024-01-05 07:34:36 -0300

    item count in hedaer

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,10 +1,16 @@
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Badge, Navbar, Container } from "react-bootstrap";
 import { FaUser } from "react-icons/fa";
 import { MdShoppingCart } from "react-icons/md";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../store"
 
 function Header() {
+
+  const {cartItems} = useSelector((state:RootState)=>state.cart)
+  
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -19,6 +25,15 @@ function Header() {
             <Nav className="ms-auto">
               <Nav.Link as={Link} to="/cart">
                 <MdShoppingCart /> Cart
+
+                {
+                  cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{marginLeft : "5px"}}>
+                      {cartItems.reduce((acc , item)=>acc + item.qty, 0)}
+
+                    </Badge>
+                  )
+                }
               </Nav.Link>
 
               <Nav.Link as={Link} to="/login">
