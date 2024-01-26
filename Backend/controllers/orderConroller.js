5eb26376b845eb03cefaa38ae756bb995079a9b1
isaak gz <yesakgz12@gmail.com>
2024-01-25 22:53:53 -0300

    Update to order controller

--- Backend/controllers/orderConroller.js
+++ Backend/controllers/orderConroller.js
@@ -5,24 +5,61 @@ import Order from "../models/orderModel.js";
 //@route POST /api/orders
 //@acess private
 const addOrderItem = asyncHandler(async (req, res) => {
-  res.send(" add order items");
-  // const order = new Order({});
+  const {
+    orderItems,
+    itemsPrice,
+    shippingPrice,
+    paymentMethod,
+    taxPrice,
+    totalPrice,
+    shippingAdress,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    return res.status(400).json({ error: "No item in the cart" });
+    throw new Error("No item in the cart");
+  } else {
+    //build a new order and save it to database
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      userId: req.user._id,
+      itemsPrice,
+      shippingPrice,
+      paymentMethod,
+      taxPrice,
+      totalPrice,
+      shippingAdress,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 // @desc get logged in users orders
 //@route Get /api/orders/myorders
 //@acess private
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send(" get my orders");
-  // const order = new Order({});
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // @desc get order by id
 //@route Get /api/orders/:id
 //@acess private
 const getMyOrdersById = asyncHandler(async (req, res) => {
-  res.send(" get orders by id");
-  // const order = new Order({});
+  const order = await Order.findOne({ _id: req.params.id}).populate("user", "name,email");
+
+  if (order){
+    res.status(200).json(order);
+  }else{
+    res.status(404).json({error:"Order not found!"});
+    throw new Error("Order not found!")
+  }
+  
 });
 
 // @desc get update order to paid
@@ -56,4 +93,3 @@ export {
   getOrders,
   updateOrderToPaid,
 };
-
