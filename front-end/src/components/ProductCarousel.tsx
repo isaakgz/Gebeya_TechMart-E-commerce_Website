fb9bb1a7ed235e7b4b36789b352a822f0438b71e
yesak gezhagne <140894606+isaakgz@users.noreply.github.com>
2024-02-21 21:53:00 +0300

    Update ProductCarousel.tsx

--- front-end/src/components/ProductCarousel.tsx
+++ front-end/src/components/ProductCarousel.tsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom"
 function ProductCarousel() {
 
     const {data:products, isLoading, error} = useGetTopProductsQuery()
-  return isLoading ? <Loader /> : error ? <Message variant='danger'>Error fetching top rated products</Message> : (
+  return isLoading ? "" : error ? <Message variant='danger'>Error fetching top rated products</Message> : (
     <Carousel pause = "hover" className="bg-primary mb-4">
         {products?.map((product) => (
             <Carousel.Item key={product._id}>
